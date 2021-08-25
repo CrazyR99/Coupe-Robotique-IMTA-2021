296b75f2f3ae6f072e2bda0908f7b1d3a2002988
Mashbrow <maxence.nesme@live.fr>
2021-08-25 17:02:37 +0200

    MQTT Protocol for GUI, websocket server

--- AI/main.js
+++ AI/main.js
@@ -1,8 +1,31 @@
-console.log("Hello World");
+
+async function createServer() {
+
+    var express = require('express');
+    const httpServer = require('http').createServer();
+    var expressApp = express();
+    expressApp.use(express.static('./www'));
+    expressApp.listen(8080);
+    const aedes = require('aedes')()
+    const port = 8081
+    const ws = require('websocket-stream')
+
+    ws.createServer({ server: httpServer }, aedes.handle)
+
+    httpServer.listen(port, function () {
+        console.log('websocket server listening on port ', port)
+    })
+
+    aedes.on('client', function (client) {
+        console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    })
 
 
+}
 
 async function main(){
+
+    await createServer();
     //Load map
     map = require('./maps/map_2022').map
     //console.log(map)
